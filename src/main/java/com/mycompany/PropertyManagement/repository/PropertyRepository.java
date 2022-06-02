4d1283d60b57f0ed8ab05bd6ad7daf366a64975f
Akshay Sharma <Akshay@Akshay.SHARMA>
2022-06-02 22:52:56 +0530

    initial commit

---
+++ src/main/java/com/mycompany/PropertyManagement/repository/PropertyRepository.java
@@ -0,0 +1,8 @@
+package com.mycompany.PropertyManagement.repository;
+
+import com.mycompany.PropertyManagement.entity.PropertyEntity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PropertyRepository extends CrudRepository<PropertyEntity, Long> {
+
+}
