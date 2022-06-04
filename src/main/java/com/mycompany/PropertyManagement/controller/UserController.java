6e838ef32b6bbd2416134bc32337a3c9a6613ddf
Akshay <akshaysharma750@gmail.com>
2022-06-04 17:14:56 +0530

    implemented logging with logback and slf4j

--- src/main/java/com/mycompany/PropertyManagement/controller/UserController.java
+++ src/main/java/com/mycompany/PropertyManagement/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.mycompany.PropertyManagement.controller;
 
-import com.mycompany.PropertyManagement.dto.PropertyDTO;
 import com.mycompany.PropertyManagement.dto.UserDTO;
 import com.mycompany.PropertyManagement.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping("/api/v1/user")
 public class UserController {
@@ -19,7 +20,7 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/register")
-    public ResponseEntity<UserDTO> register(@RequestBody UserDTO userDTO){
+    public ResponseEntity<UserDTO> register(@Valid @RequestBody UserDTO userDTO){
 
         userDTO = userService.register(userDTO);
 
@@ -28,7 +29,7 @@ public class UserController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<UserDTO> login(@RequestBody UserDTO userDTO){
+    public ResponseEntity<UserDTO> login(@Valid @RequestBody UserDTO userDTO){
 
         userDTO = userService.login(userDTO.getOwnerEmail(), userDTO.getPassword());
         return new ResponseEntity<>(userDTO, HttpStatus.OK);
